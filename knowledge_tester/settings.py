eb2d8facaf490c842a8c4fe0892270a4ebb2506b
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-01-20 04:33:44 +0600

    fifth

--- knowledge_tester/settings.py
+++ knowledge_tester/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = "django-insecure-e)0(mhv4em%#djav$*!#ik2_%3x6(9rk&^mp5mc#)=arq$gfi-
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*.mydomain.com", "https://*.127.0.0.1"]
 
 
 # Application definition
